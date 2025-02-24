392ffe92c3cbcada9b827e66fe7fc9044eecbb21
Niraj Kumar <nirajgupta7481@gmail.com>
2025-02-24 16:54:08 +0530

    [Niraj] Add . basic rest calls added and tested

--- src/main/java/com/bridgelabz/employeepayrollapp/controller/EmployeeController.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/controller/EmployeeController.java
@@ -1,41 +1,21 @@
 package com.bridgelabz.employeepayrollapp.controller;
 
 import com.bridgelabz.employeepayrollapp.model.Employee;
-import com.bridgelabz.employeepayrollapp.service.EmployeeService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @RestController
-@RequestMapping("/employees")
+@RequestMapping("/employee")
 public class EmployeeController {
-    @Autowired
-    private EmployeeService service;
 
-    @GetMapping
-    public List<Employee> getAllEmployees() {
-        return service.getAllEmployees();
+    @GetMapping("/welcome")
+    public String welcomeMessage() {
+        return "Welcome to Employee Payroll App!";
     }
 
-    @GetMapping("/{id}")
-    public Employee getEmployeeById(@PathVariable Long id) {
-        return service.getEmployeeById(id);
-    }
-
-    @PostMapping
-    public Employee createEmployee(@RequestBody Employee employee) {
-        return service.saveEmployee(employee);
-    }
-
-    @PutMapping("/{id}")
-    public Employee updateEmployee(@PathVariable Long id, @RequestBody Employee updatedEmployee) {
-        return service.updateEmployee(id, updatedEmployee);
-    }
-
-    @DeleteMapping("/{id}")
-    public void deleteEmployee(@PathVariable Long id) {
-        service.deleteEmployee(id);
+    @PostMapping("/add")
+    public Employee addEmployee(@RequestBody Employee employee) {
+        return employee;
     }
 }
+
 
--- src/main/java/com/bridgelabz/employeepayrollapp/model/Employee.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/model/Employee.java
@@ -1,25 +1,33 @@
 package com.bridgelabz.employeepayrollapp.model;
 
-import jakarta.persistence.*;
-import lombok.*;
-
-@Entity
-@Table(name = "employees")
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
 public class Employee {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @Column(nullable = false)
     private String name;
-
-    @Column(nullable = false)
     private double salary;
 
-    private String department;
+    public Employee() {
+    }
+
+    public Employee(String name, double salary) {
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
 }
+
+
 
